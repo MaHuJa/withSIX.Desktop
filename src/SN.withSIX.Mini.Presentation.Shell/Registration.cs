da5ae2041b603b2e8ec2f8f050140cd7a859d976
Patrick Roza <roza@sixnetworks.net>
2015-11-15 12:28:44 +0100

    nfo

--- src/SN.withSIX.Mini.Presentation.Shell/Registration.cs
+++ src/SN.withSIX.Mini.Presentation.Shell/Registration.cs
@@ -4,5 +4,6 @@
     // Register with admin rights:
     // "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\regasm"  /register /codebase "E:\projects\SN\withSIX.Desktop\src\SN.withSIX.Mini.Presentation.Shell\bin\Debug\SN.withSIX.Mini.Presentation.Shell.dll"
     // "C:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm"  /register /codebase "E:\projects\SN\withSIX.Desktop\src\SN.withSIX.Mini.Presentation.Shell\bin\Debug\SN.withSIX.Mini.Presentation.Shell.dll"
+    // http://www.codeproject.com/Articles/653780/NET-Shell-Extensions-Deploying-SharpShell-Servers
     class Registration {}
 }
